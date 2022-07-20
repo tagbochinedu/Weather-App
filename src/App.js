83990947c9e9ddb53d6e7779f9883b0318642f9a
Tagbo Chinedu <johntagbo2@gmail.com>
2022-07-20 10:49:51 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    const locate = async () => {
+      try {
+        const fetchLocation = await fetch(
+          "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyD_rbHJo6ug-X95KgkJZtPHI_5mlC9AgH0"
+        );
+        const response = await fetchLocation.json();
+        console.log(response);
+      } catch (error) {
+        alert(error.message);
+        console.log(error)
+      }
+    };
+
+    locate();
+  }, []);
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p></p>
+    </>
+  );
 }
