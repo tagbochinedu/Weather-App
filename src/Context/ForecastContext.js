f05380296a2dc869653c48fc76095083ac028871
Tagbo Chinedu <johntagbo2@gmail.com>
2022-07-27 15:53:56 +0100

    searchbar

--- src/Context/ForecastContext.js
+++ src/Context/ForecastContext.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 const ForecastContext = React.createContext();
 
 export function useAuth() {
@@ -8,12 +8,46 @@ export function useAuth() {
 export function AuthProvider({ children }) {
   const [latitude, setLatitude] = useState("");
   const [longitude, setLongitude] = useState("");
-  const [locationWeather, setLocationWeather] = useState([])
+  const [locationWeather, setLocationWeather] = useState([]);
   const [weeklyWeather, setWeeklyWeather] = useState([]);
-  const [loading, setLoading] = useState(true)
- 
+  const [loading, setLoading] = useState(true);
+  const [city, setCity] = useState("");
+  const [data, setData] = useState();
+  const [errorText, setErrorText] = useState('')
+  const [error, setError] = useState(false)
+  const [searchLoader, setSearchLoader] = useState(false);
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+      );
+      const res = await response.json();
+      const resData = [
+        {
+          name: res.name,
+          temp: res.main.temp,
+          min_temp: res.main.temp_min,
+          max_temp: res.main.temp_max,
+          country: res.sys.country,
+          sunrise: res.sys.sunrise,
+          sunset: res.sys.sunset,
+          weather: res.weather[0].main,
+          weatherDesc: res.weather[0].description,
+        },
+      ];
+      setData(resData);
+      setSearchLoader(false);
+      console.log(res);
+    } catch(error) {
+      console.log(error.message)
+      setError(true)
+    }
+  };
+
   useEffect(() => {
-    const API_KEY = process.env.REACT_APP_API_KEY;
     navigator.geolocation.getCurrentPosition((position) => {
       setLatitude(position.coords.latitude);
       setLongitude(position.coords.longitude);
@@ -29,7 +63,7 @@ export function AuthProvider({ children }) {
         );
         const res = await response.json();
         const res1 = await response1.json();
-          
+
         const months = [
           "Jan",
           "Feb",
@@ -44,7 +78,7 @@ export function AuthProvider({ children }) {
           "Nov",
           "Dec",
         ];
-        const days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat" ];
+        const days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
         const DateCalc = (timing) => {
           let time = "";
           if (timing) {
@@ -54,13 +88,13 @@ export function AuthProvider({ children }) {
           }
           const month = months[time.getMonth()];
           const date = time.getDate();
-          const day = days[time.getDay()]
+          const day = days[time.getDay()];
           return `${day}, ${month} ${date}`;
         };
 
         const TimeCalc = (timing) => {
           const time = new Date(timing * 1000);
-          let hours = (time.getUTCHours()+1).toString();
+          let hours = (time.getUTCHours() + 1).toString();
           const minutes = time.getMinutes().toString();
           if (hours > 12) {
             hours -= 12;
@@ -128,20 +162,28 @@ export function AuthProvider({ children }) {
             weather: res1.daily[6].weather[0].main,
           },
         ];
-        setWeeklyWeather(weeklyData)
+        setWeeklyWeather(weeklyData);
         setLocationWeather(data);
-        setLoading(false)
-        console.log(data)
+        setLoading(false);
+        console.log(data);
         console.log(res1);
       } catch (error) {
         console.log(error);
       }
     };
     fetchWeather();
-  }, [latitude, longitude]);
+  }, [latitude, longitude, API_KEY]);
 
-
-  const value = {locationWeather, weeklyWeather, loading};
+  const value = {
+    locationWeather,
+    weeklyWeather,
+    loading,
+    submitHandler,
+    setCity,
+    data,
+    searchLoader,
+    setSearchLoader
+  };
   return (
     <ForecastContext.Provider value={value}>
       {children}
